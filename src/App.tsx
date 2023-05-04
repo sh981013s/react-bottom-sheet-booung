40ccb8b0b4cf2d317fb349b4b896b2b0b8dd6986
sh981013s <sh981013s@gmail.com>
2023-05-04 15:54:12 +0900

    feat: 예시 코드 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import MyBottomSheet from './lib/components/MyBottomSheet';
+import { BottomSheetProvider, useBottomSheet } from './lib';
+
+interface MyModalProps {
+  onClose: () => void;
+}
+
+const MyModal = ({ onClose }: MyModalProps) => {
+  return (
+    <div>
+      <h1>My Modal</h1>
+      <p>This is a sample modal content.</p>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+};
+
+const ModalButton = () => {
+  const { showBottomSheet } = useBottomSheet();
+
+  const showModal = () => {
+    showBottomSheet(MyModal, true);
+  };
+
+  return <button onClick={showModal}>Show Modal</button>;
+};
 
 const App = () => {
   return (
-    <MyBottomSheet trigger={<button>open BottomSheet</button>}>
-      <h2>제목</h2>
-      <p>내용</p>
-    </MyBottomSheet>
+    <BottomSheetProvider>
+      <div>
+        <h1>React Bottom Sheet Example</h1>
+        <ModalButton />
+      </div>
+    </BottomSheetProvider>
   );
 };
 
